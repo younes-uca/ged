ecfa7c0c2787dcf631cd763adabdce46c48c4e6f
TOUFGA IBRAHIM <itoufga@leyton.com>
2023-07-29 18:45:35 +0100

    Minio dev : 1 - add configuration to use env variables for minio and datasource password 2 - configure minio client to create a default bucket if not exists and enable versioning in this bucket 3 - add the possibility to download all the files inside a specific directory inside the bucket 4 - add create directory inside a bucket

--- minio/src/main/java/ma/sir/ged/service/facade/admin/MinIOService.java
+++ minio/src/main/java/ma/sir/ged/service/facade/admin/MinIOService.java
@@ -6,14 +6,18 @@ import java.util.List;
 
 public interface MinIOService {
 
-    int bucketExists(String bucket);
+    Boolean bucketExists(String bucket);
 
     int upload(MultipartFile file, String bucket);
 
     int saveBucket(String bucket);
 
     List<String> findAllDocuments(String bucket);
+    List<String> findAllDocuments(String bucket, String path);
 
     byte[] downloadAllDocumentsAsZip(String bucket);
+    byte[] downloadAllDocumentsAsZip(String bucket, String path);
+
+    int createDirectory(String bucket, String path);
 
 }
