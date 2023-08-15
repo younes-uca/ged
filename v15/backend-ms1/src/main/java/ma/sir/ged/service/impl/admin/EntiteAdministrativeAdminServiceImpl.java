92ce203c0f34e02cfe921db1737e35ac96c89cfa
younes-uca <y.zouani@uca.ma>
2023-08-15 16:40:38 +0200

    ajout entite admin implique ajout groupe

--- v15/backend-ms1/src/main/java/ma/sir/ged/service/impl/admin/EntiteAdministrativeAdminServiceImpl.java
+++ v15/backend-ms1/src/main/java/ma/sir/ged/service/impl/admin/EntiteAdministrativeAdminServiceImpl.java
@@ -1,104 +1,122 @@
 package ma.sir.ged.service.impl.admin;
 
-import ma.sir.ged.service.facade.open.MinIOService;
 import ma.sir.ged.bean.core.EntiteAdministrative;
+import ma.sir.ged.bean.core.Groupe;
+import ma.sir.ged.bean.core.GroupeUtilisateur;
+import ma.sir.ged.bean.core.Utilisateur;
 import ma.sir.ged.bean.history.EntiteAdministrativeHistory;
 import ma.sir.ged.dao.criteria.core.EntiteAdministrativeCriteria;
 import ma.sir.ged.dao.criteria.history.EntiteAdministrativeHistoryCriteria;
 import ma.sir.ged.dao.facade.core.EntiteAdministrativeDao;
 import ma.sir.ged.dao.facade.history.EntiteAdministrativeHistoryDao;
 import ma.sir.ged.dao.specification.core.EntiteAdministrativeSpecification;
-import ma.sir.ged.service.facade.admin.EntiteAdministrativeAdminService;
+import ma.sir.ged.service.facade.admin.*;
 import ma.sir.ged.zynerator.service.AbstractServiceImpl;
 import ma.sir.ged.zynerator.util.ListUtil;
-import org.springframework.stereotype.Service;
-import java.util.List;
-import java.util.ArrayList;
-
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
-import ma.sir.ged.bean.core.Utilisateur;
-
-import ma.sir.ged.service.facade.admin.UtilisateurAdminService ;
-import ma.sir.ged.service.facade.admin.EntiteAdministrativeTypeAdminService ;
-
-
-
 import java.util.List;
+
 @Service
-public class EntiteAdministrativeAdminServiceImpl extends AbstractServiceImpl<EntiteAdministrative,EntiteAdministrativeHistory, EntiteAdministrativeCriteria, EntiteAdministrativeHistoryCriteria, EntiteAdministrativeDao,
-EntiteAdministrativeHistoryDao> implements EntiteAdministrativeAdminService {
+public class EntiteAdministrativeAdminServiceImpl extends AbstractServiceImpl<EntiteAdministrative, EntiteAdministrativeHistory, EntiteAdministrativeCriteria, EntiteAdministrativeHistoryCriteria, EntiteAdministrativeDao,
+        EntiteAdministrativeHistoryDao> implements EntiteAdministrativeAdminService {
 
 
     @Transactional(propagation = Propagation.REQUIRED, rollbackFor = Exception.class, readOnly = false)
     public EntiteAdministrative create(EntiteAdministrative t) {
+        Groupe groupe = constructGroup(t);
         super.create(t);
+        groupeService.create(groupe);
         if (t.getUtilisateurs() != null) {
-                t.getUtilisateurs().forEach(element-> {
-                    element.setEntiteAdministrative(t);
-                    utilisateurService.create(element);
+            t.getUtilisateurs().forEach(element -> {
+                element.setEntiteAdministrative(t);
+                GroupeUtilisateur groupeUtilisateur = new GroupeUtilisateur();
+                groupeUtilisateur.setUtilisateur(element);
+                utilisateurService.create(element);
+                groupeUtilisateur.setGroupe(groupe);
+                groupeUtilisateurService.create(groupeUtilisateur);
             });
         }
+
         return t;
     }
 
-    public EntiteAdministrative findWithAssociatedLists(Long id){
+    private Groupe constructGroup(EntiteAdministrative t) {
+        Groupe groupe = new Groupe();
+        groupe.setCode(t.getCode());
+        groupe.setLibelle(t.getLibelle());
+
+        return groupe;
+    }
+
+
+    public EntiteAdministrative findWithAssociatedLists(Long id) {
         EntiteAdministrative result = dao.findById(id).orElse(null);
-        if(result!=null && result.getId() != null) {
+        if (result != null && result.getId() != null) {
             result.setUtilisateurs(utilisateurService.findByEntiteAdministrativeId(id));
         }
         return result;
     }
+
     @Transactional
     public void deleteAssociatedLists(Long id) {
         utilisateurService.deleteByEntiteAdministrativeId(id);
     }
 
 
-    public void updateWithAssociatedLists(EntiteAdministrative entiteAdministrative){
-    if(entiteAdministrative !=null && entiteAdministrative.getId() != null){
-            List<List<Utilisateur>> resultUtilisateurs= utilisateurService.getToBeSavedAndToBeDeleted(utilisateurService.findByEntiteAdministrativeId(entiteAdministrative.getId()),entiteAdministrative.getUtilisateurs());
+    public void updateWithAssociatedLists(EntiteAdministrative entiteAdministrative) {
+        if (entiteAdministrative != null && entiteAdministrative.getId() != null) {
+            List<List<Utilisateur>> resultUtilisateurs = utilisateurService.getToBeSavedAndToBeDeleted(utilisateurService.findByEntiteAdministrativeId(entiteAdministrative.getId()), entiteAdministrative.getUtilisateurs());
             utilisateurService.delete(resultUtilisateurs.get(1));
             ListUtil.emptyIfNull(resultUtilisateurs.get(0)).forEach(e -> e.setEntiteAdministrative(entiteAdministrative));
-            utilisateurService.update(resultUtilisateurs.get(0),true);
-    }
-    }
-
-    public EntiteAdministrative findByReferenceEntity(EntiteAdministrative t){
-        return  dao.findByCode(t.getCode());
+            utilisateurService.update(resultUtilisateurs.get(0), true);
+        }
     }
 
-    public List<EntiteAdministrative> findByEntiteAdministrativeParentId(Long id){
+    public EntiteAdministrative findByReferenceEntity(EntiteAdministrative t) {
+        return dao.findByCode(t.getCode());
+    }
+
+    public List<EntiteAdministrative> findByEntiteAdministrativeParentId(Long id) {
         return dao.findByEntiteAdministrativeParentId(id);
     }
-    public int deleteByEntiteAdministrativeParentId(Long id){
+
+    public int deleteByEntiteAdministrativeParentId(Long id) {
         return dao.deleteByEntiteAdministrativeParentId(id);
     }
-    public List<EntiteAdministrative> findByChefId(Long id){
+
+    public List<EntiteAdministrative> findByChefId(Long id) {
         return dao.findByChefId(id);
     }
-    public int deleteByChefId(Long id){
+
+    public int deleteByChefId(Long id) {
         return dao.deleteByChefId(id);
     }
-    public List<EntiteAdministrative> findByEntiteAdministrativeTypeId(Long id){
+
+    public List<EntiteAdministrative> findByEntiteAdministrativeTypeId(Long id) {
         return dao.findByEntiteAdministrativeTypeId(id);
     }
-    public int deleteByEntiteAdministrativeTypeId(Long id){
+
+    public int deleteByEntiteAdministrativeTypeId(Long id) {
         return dao.deleteByEntiteAdministrativeTypeId(id);
     }
 
 
-
     public void configure() {
-        super.configure(EntiteAdministrative.class,EntiteAdministrativeHistory.class, EntiteAdministrativeHistoryCriteria.class, EntiteAdministrativeSpecification.class);
+        super.configure(EntiteAdministrative.class, EntiteAdministrativeHistory.class, EntiteAdministrativeHistoryCriteria.class, EntiteAdministrativeSpecification.class);
     }
+
     @Autowired
-    private UtilisateurAdminService utilisateurService ;
+    private GroupeAdminService groupeService;
     @Autowired
-    private EntiteAdministrativeTypeAdminService entiteAdministrativeTypeService ;
+    private GroupeUtilisateurAdminService groupeUtilisateurService;
+    @Autowired
+    private UtilisateurAdminService utilisateurService;
+    @Autowired
+    private EntiteAdministrativeTypeAdminService entiteAdministrativeTypeService;
 
     public EntiteAdministrativeAdminServiceImpl(EntiteAdministrativeDao dao, EntiteAdministrativeHistoryDao historyDao) {
         super(dao, historyDao);
